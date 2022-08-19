c78fc3e3cf99becc0cda987b077c911ba1b9ae3d
yellowsea <yunhai0644@163.com>
2022-08-20 00:34:00 +0800

    test: host Page skip Editor

--- apps/editor/src/pages/index.vue
+++ apps/editor/src/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
     <div>index.vue</div>
+    <a-button type="primary" @click="onEditor">Editor</a-button>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+const route = useRouter();
+
+function onEditor() {
+    route.push("/editor");
+}
 </script>
 
 <style>
