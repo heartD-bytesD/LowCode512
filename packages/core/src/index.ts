2e8520b1810e0b47dcebd4295db842f465094ca4
rhxie <xieruihong@corp.netease.com>
2023-12-25 18:44:51 +0800

    chore: 20231225

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,10 @@
 export default {
-    'lc-button': () => import("./lc-button"),
-    'lc-image': () => import("./lc-image")
+    id: 'core', // 唯一id，支持同id的包对此进行扩展
+    version: '0.0.3', // 版本号
+    name: '基础组件', // 分类名
+    type: 'core', // 分类
+    components: {
+        'lc-button': () => import("./lc-button"),
+        'lc-image': () => import("./lc-image"),
+    }
 }
