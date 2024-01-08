a2a5447e679b96faaa5562cbcb6e52f7a7cea617
rhxie <xieruihong@corp.netease.com>
2024-01-08 19:27:01 +0800

    feat: JSX/TSX for vite

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue';
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import pkg from './package.json'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vueJsx()],
     build: {
         lib: {
             entry: './src/index.ts',
